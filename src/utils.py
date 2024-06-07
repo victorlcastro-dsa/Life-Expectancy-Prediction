8cbb0510ee55ffb73c157a97efb6c0857481d90d
Victor Lopes de Castro <victorlcastro.dsa@gmail.com>
2024-06-07 16:44:54 -0300

    Basic Project Structure | Logging | Exception | EDA&MODELTRAINING.ipynb creation

---
+++ src/utils.py
@@ -0,0 +1,5 @@
+'''
+Short description of utils.py:
+-Using utils.py facilitates organizing utility functions, constants, or classes in a centralized location, enhancing code maintainability and readability by avoiding code duplication and promoting reusability.
+
+'''
