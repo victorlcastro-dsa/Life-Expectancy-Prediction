61e9aadc7c5155d2e02590856071b2859ea66ed7
Victor Lopes de Castro <victorlcastro.dsa@gmail.com>
2024-06-07 16:51:46 -0300

    setup.py description changes

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 ''' 
-Description of setup.py
+Short description of setup.py:
+-The setup.py file configures the distribution of Python packages, specifying metadata, dependencies, and included files.
+-It facilitates installation with tools like pip and defines executable scripts. Essential for creating easily distributable and installable packages.
 
-The setup.py file configures the distribution of Python packages, specifying metadata, dependencies, and included files.
-It facilitates installation with tools like pip and defines executable scripts. Essential for creating easily distributable and installable packages.
+
+Short description of libraries used:
+-Python setuptools library provides utilities for packaging, distributing, and installing Python projects, simplifying the management of dependencies and project setup.
+-Python typing library provides support for type hints, allowing developers to statically declare types for variables, function parameters, and return values, enhancing code readability and maintainability.
 
 '''
 # Importing functions for setup configuration
