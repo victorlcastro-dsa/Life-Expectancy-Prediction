fce5063ae81907d375228318783f3b5d93127900
Asafe Oliveira <asafe.anjos@trustly.com>
2024-06-17 19:22:07 -0300

    Improvement to the setup file and removed the requirements.txt file, which is not useful

--- setup.py
+++ setup.py
@@ -9,36 +9,22 @@ Short description of libraries used:
 -Python typing library provides support for type hints, allowing developers to statically declare types for variables, function parameters, and return values, enhancing code readability and maintainability.
 
 '''
-# Importing functions for setup configuration
 from setuptools import setup, find_packages
-from typing import List
 
-# Function creation and adjustment for getting the requirements
-HYPEN_E_DOT = '-e .' 
-
-def get_requirements(file_path:str) -> List[str]:
-    '''
-    This function will return the list of requirements
-    '''
-    requirements = []
-    with open(file_path) as file_obj:
-        requirements = file_obj.readlines()
-        requirements = [req.replace('\n', '') for req in requirements]
-        
-        if HYPEN_E_DOT in requirements:
-            requirements.remove(HYPEN_E_DOT)
-    
-    return requirements
-
+# Command to install packages
+# pip3 install -e .
+requirements = [
+    "pandas==2.2.2",
+    "scikit-learn==1.5.0"
+]
 
 # Setup configuration
 setup(
-    name = 'LifeExpectancy',
-    version = '0.0.1',
-    author = 'Victor L. Castro',
-    author_email = 'victorlcastro.dsa@gmail.com',
-    packages = find_packages(),
-    install_requires = get_requirements('requirements.txt')
-    
-    
+    name='LifeExpectancy',
+    version='0.0.1',
+    author='Victor L. Castro',
+    author_email='victorlcastro.dsa@gmail.com',
+    packages=find_packages(),
+    install_requires = requirements,
+    python_requires='>=3.11',
 )
